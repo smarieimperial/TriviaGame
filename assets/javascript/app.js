d5af36eaf462299758852f120e021be88ab86564
stephanie <stephanie99tech@gmail.com>
2018-07-05 01:47:01 -0700

    finished most of hw 5

---
+++ assets/javascript/app.js
@@ -0,0 +1,47 @@
+score = 0;
+incorrect = 0;
+unanswered = 0;
+
+function addScore() {
+    document.write("All Done!");
+    document.write("<p>Correct Answers: " + score + "</p>");
+    document.write("<p>Incorrect Answers: " + incorrect + "</p>");
+    document.write("<p>Unsanswered: " + unanswered + "</p>");
+
+    $('#done').on('click', function(){
+        clearInterval(timer);
+        seconds = 0;
+        $('#countdown').text(seconds);
+    });
+}
+
+// code for the countdown timer below. Timer seconds starts at 10 for testing.
+    var seconds = 10;
+    var timer;
+        // div id 'countdown' prints to screen the number in the variable named seconds.
+		$('#countdown').text(seconds);
+
+		function startTimer(){
+			seconds--;
+            $('#countdown').text(seconds);
+            if (seconds < 0) {
+                clearInterval(timer);
+                alert("Time's up!");
+                seconds = 10;
+			    $('#countdown').text(seconds);
+                addScore();
+            }
+		}
+
+		$('#start').on('click', function(){
+			timer = setInterval(startTimer, 1000);
+		});
+
+        // added this stop button for testing.
+		$('#stop').on('click', function(){
+			clearInterval(timer);
+			seconds = 10;
+			$('#countdown').text(seconds);
+        });
+        
+    
